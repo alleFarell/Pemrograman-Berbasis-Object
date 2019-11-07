a6b782ef285673d98ca1404180b2422bc25b199f
AaRai27 <46803391+AaRai27@users.noreply.github.com>
2019-11-07 15:22:41 +0700

    Update Soal1.java

--- MOD1/soal1/src/soal1/Soal1.java
+++ MOD1/soal1/src/soal1/Soal1.java
@@ -1,4 +1,5 @@
 /*
+    INI BUAT NYOBA UPDATE ATAU NGGAK
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
